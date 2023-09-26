b129638069a1666c39b449a3bf0a26d4b75ad88e
LIiliILf <3162441851@qq.com>
2023-09-26 21:06:29 +0800

    version1

---
+++ app.js
@@ -0,0 +1,435 @@
+// initial value
+
+// screens
+//分别获取了具有特定 id 的HTML元素
+const start_screen = document.querySelector('#start-screen');
+const game_screen = document.querySelector('#game-screen');
+const pause_screen = document.querySelector('#pause-screen');
+const result_screen = document.querySelector('#result-screen');
+// ----------
+const cells = document.querySelectorAll('.main-grid-cell');
+
+const name_input = document.querySelector('#input-name');
+
+const number_inputs = document.querySelectorAll('.number');
+
+const player_name = document.querySelector('#player-name');
+const game_level = document.querySelector('#game-level');
+const game_time = document.querySelector('#game-time');
+
+const result_time = document.querySelector('#result-time');
+
+let level_index = 0;
+let level = CONSTANT.LEVEL[level_index];
+
+let timer = null;
+let pause = false;
+let seconds = 0;
+
+let su = undefined;
+let su_answer = undefined;
+
+let selected_cell = -1;
+
+// --------
+
+const getGameInfo = () => JSON.parse(localStorage.getItem('game'));
+
+// add space for each 9 cells
+const initGameGrid = () => {
+    let index = 0;
+
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE,2); i++) {
+        let row = Math.floor(i/CONSTANT.GRID_SIZE);
+        let col = i % CONSTANT.GRID_SIZE;
+        if (row === 2 || row === 5) cells[index].style.marginBottom = '10px';
+        if (col === 2 || col === 5) cells[index].style.marginRight = '10px';
+
+        index++;
+    }
+}
+// ----------------
+//saveNametoLocal and getNameFromLocal
+const setPlayerName = (name) => localStorage.setItem('player_name', name);
+const getPlayerName = () => localStorage.getItem('player_name');
+
+const showTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8);
+
+const clearSudoku = () => {
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        cells[i].innerHTML = '';
+        cells[i].classList.remove('filled');
+        cells[i].classList.remove('selected');
+    }
+}
+
+const initSudoku = () => {
+    // clear old sudoku
+    clearSudoku();
+    resetBackground();
+    // generate sudoku puzzle
+    // 存储在 su 变量中。
+
+    su = sudokuGen(level);
+    su_answer = [...su.question];
+    seconds = 0;
+
+    saveGameInfo();
+
+    // show sudoku to div
+    //遍历所有格子，将格子的data-value属性设置为对应数独的值。
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        let row = Math.floor(i / CONSTANT.GRID_SIZE);
+        let col = i % CONSTANT.GRID_SIZE;
+        
+        cells[i].setAttribute('data-value', su.question[row][col]);
+
+        if (su.question[row][col] !== 0) {
+            cells[i].classList.add('filled');
+            cells[i].innerHTML = su.question[row][col];
+        }
+    }
+}
+
+const loadSudoku = () => {
+    let game = getGameInfo();
+
+    game_level.innerHTML = CONSTANT.LEVEL_NAME[game.level];
+
+    su = game.su;
+
+    su_answer = su.answer;
+
+    seconds = game.seconds;
+    game_time.innerHTML = showTime(seconds);
+
+    level_index = game.level;
+
+    // show sudoku to div
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        let row = Math.floor(i / CONSTANT.GRID_SIZE);
+        let col = i % CONSTANT.GRID_SIZE;
+        
+        cells[i].setAttribute('data-value', su_answer[row][col]);
+        cells[i].innerHTML = su_answer[row][col] !== 0 ? su_answer[row][col] : '';
+        if (su.question[row][col] !== 0) {
+            cells[i].classList.add('filled');
+        }
+    }
+}
+//当鼠标悬停在格子上时，为一些相关的格子添加hover类突出显示，提高可玩性。
+const hoverBackground = (index) => {
+    let row = Math.floor(index / CONSTANT.GRID_SIZE);
+    let col = index % CONSTANT.GRID_SIZE;
+
+    let box_start_row = row - row % 3;
+    let box_start_col = col - col % 3;
+
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+            let cell = cells[9 * (box_start_row + i) + (box_start_col + j)];
+            cell.classList.add('hover');
+        }
+    }
+
+    let step = 9;
+    while (index - step >= 0) {
+        cells[index - step].classList.add('hover');
+        step += 9;
+    }
+
+    step = 9;
+    while (index + step < 81) {
+        cells[index + step].classList.add('hover');
+        step += 9;
+    }
+
+    step = 1;
+    while (index - step >= 9*row) {
+        cells[index - step].classList.add('hover');
+        step += 1;
+    }
+
+    step = 1;
+     while (index + step < 9*row + 9) {
+        cells[index + step].classList.add('hover');
+        step += 1;
+    }
+}
+
+const resetBackground = () => {
+    cells.forEach(e => e.classList.remove('hover'));
+}
+
+const checkErr = (value) => {
+    const addErr = (cell) => {
+        if (parseInt(cell.getAttribute('data-value')) === value) {
+            cell.classList.add('err');
+            cell.classList.add('cell-err');
+            setTimeout(() => {
+                cell.classList.remove('cell-err');
+            }, 500);
+        }
+    }
+
+    let index = selected_cell;
+
+    let row = Math.floor(index / CONSTANT.GRID_SIZE);
+    let col = index % CONSTANT.GRID_SIZE;
+
+    let box_start_row = row - row % 3;
+    let box_start_col = col - col % 3;
+
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+            let cell = cells[9 * (box_start_row + i) + (box_start_col + j)];
+            if (!cell.classList.contains('selected')) addErr(cell);
+        }
+    }
+
+    let step = 9;
+    while (index - step >= 0) {
+        addErr(cells[index - step]);
+        step += 9;
+    }
+
+    step = 9;
+    while (index + step < 81) {
+        addErr(cells[index + step]);
+        step += 9;
+    }
+
+    step = 1;
+    while (index - step >= 9*row) {
+        addErr(cells[index - step]);
+        step += 1;
+    }
+
+    step = 1;
+    while (index + step < 9*row + 9) {
+        addErr(cells[index + step]);
+        step += 1;
+    }
+}
+//移除错误的格子
+const removeErr = () => cells.forEach(e => e.classList.remove('err'));
+
+const saveGameInfo = () => {
+    let game = {
+        level: level_index,
+        seconds: seconds,
+        su: {
+            original: su.original,
+            question: su.question,
+            answer: su_answer
+        }
+    }
+    localStorage.setItem('game', JSON.stringify(game));
+}
+
+const removeGameInfo = () => {
+    localStorage.removeItem('game');
+    document.querySelector('#button-continue').style.display = 'none';
+}
+
+const isGameWin = () => sudokuCheck(su_answer);
+
+const showResult = () => {
+    clearInterval(timer);
+    result_screen.classList.add('active');
+    result_time.innerHTML = showTime(seconds);
+}
+
+const initNumberInputEvent = () => {
+    number_inputs.forEach((e, index) => {
+        e.addEventListener('click', () => {
+            if (!cells[selected_cell].classList.contains('filled')) {
+                cells[selected_cell].innerHTML = index + 1;
+                cells[selected_cell].setAttribute('data-value', index + 1);
+                // add to answer
+                let row = Math.floor(selected_cell / CONSTANT.GRID_SIZE);
+                let col = selected_cell % CONSTANT.GRID_SIZE;
+                su_answer[row][col] = index + 1;
+                // save game
+                saveGameInfo()
+                // -----
+                removeErr();
+                checkErr(index + 1);
+                cells[selected_cell].classList.add('zoom-in');
+                setTimeout(() => {
+                    cells[selected_cell].classList.remove('zoom-in');
+                }, 500);
+
+                // check game win
+                if (isGameWin()) {
+                    removeGameInfo();
+                    showResult();
+                }
+                // ----
+            }
+        })
+    })
+}
+
+const initCellsEvent = () => {
+    cells.forEach((e, index) => {
+        e.addEventListener('click', () => {
+            if (!e.classList.contains('filled')) {
+                cells.forEach(e => e.classList.remove('selected'));
+
+                selected_cell = index;
+                e.classList.remove('err');
+                e.classList.add('selected');
+                resetBackground();
+                hoverBackground(index);
+            }
+        })
+    })
+}
+
+const startGame = () => {
+    start_screen.classList.remove('active');
+    game_screen.classList.add('active');
+
+    player_name.innerHTML = name_input.value.trim();
+    setPlayerName(name_input.value.trim());
+
+    game_level.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+
+    showTime(seconds);
+    initSudoku();
+
+    timer = setInterval(() => {
+        if (!pause) {
+            seconds = seconds + 1;
+            game_time.innerHTML = showTime(seconds);
+        }
+    }, 1000);
+}
+
+
+
+
+const returnStartScreen = () => {
+    clearInterval(timer);
+    pause = false;
+    seconds = 0;
+    start_screen.classList.add('active');
+    game_screen.classList.remove('active');
+    pause_screen.classList.remove('active');
+    result_screen.classList.remove('active');
+}
+
+// add button event
+document.querySelector('#button-level').addEventListener('click', (e) => {
+    level_index = level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
+    level = CONSTANT.LEVEL[level_index];
+    e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+});
+
+document.querySelector('#button-play').addEventListener('click', () => {
+    if (name_input.value.trim().length > 0) {
+        initSudoku();
+        startGame();
+    } else {
+        name_input.classList.add('input-err');
+        setTimeout(() => {
+            name_input.classList.remove('input-err');
+            name_input.focus();
+        }, 500);
+    }
+});
+//处理游戏的暂停、继续、重新开始、格子的
+document.querySelector('#button-continue').addEventListener('click', () => {
+    if (name_input.value.trim().length > 0) {
+        loadSudoku();
+        startGame();
+    } else {
+        name_input.classList.add('input-err');
+        setTimeout(() => {
+            name_input.classList.remove('input-err');
+            name_input.focus();
+        }, 500);
+    }
+});
+
+document.querySelector('#button-pause').addEventListener('click', () => {
+    pause_screen.classList.add('active');
+    pause = true;
+});
+
+document.querySelector('#button-resume').addEventListener('click', () => {
+    pause_screen.classList.remove('active');
+    pause = false;
+});
+
+document.querySelector('#button-new-game').addEventListener('click', () => {
+    returnStartScreen();
+});
+
+document.querySelector('#button-new-game-2').addEventListener('click', () => {
+    console.log('object')
+    returnStartScreen();
+});
+
+document.querySelector('#button-delete').addEventListener('click', () => {
+    cells[selected_cell].innerHTML = '';
+    cells[selected_cell].setAttribute('data-value', 0);
+
+    let row = Math.floor(selected_cell / CONSTANT.GRID_SIZE);
+    let col = selected_cell % CONSTANT.GRID_SIZE;
+
+    su_answer[row][col] = 0;
+
+    removeErr();
+})
+// -------------
+
+const init = () => {
+    
+    const game = getGameInfo();
+
+    document.querySelector('#button-continue').style.display = game ? 'grid' : 'none';
+
+    initGameGrid();
+    initCellsEvent();
+    initNumberInputEvent();
+
+    if (getPlayerName()) {
+        name_input.value = getPlayerName();
+    } else {
+        name_input.focus();
+    }
+}
+
+init();
+//生成9个独立的数独谜题
+const sudokuContainers = [];
+
+for (let i = 1; i <= 9; i++) {
+    const container = document.querySelector(`#sudoku-container-${i}`);
+    const level = 0; // 设置难度级别，根据您的需求进行调整
+    const sudoku = sudokuGen(level);
+    
+    if (sudoku) {
+        // 创建数独表格并填充到容器中
+        const grid = createSudokuGrid(sudoku.question); // 这里需要一个函数来生成数独表格
+        container.appendChild(grid);
+        sudokuContainers.push(grid);
+    }
+}
+
+
+function createSudokuGrid(sudoku) {
+    const table = document.createElement('table');
+    for (let i = 0; i < 9; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 9; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = sudoku[i][j] !== 0 ? sudoku[i][j] : '';
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    return table;
+}
